26e8d8f489bdc650802b459a23a9d98c65e62f74
John0588 <John.Almirol@triosstudent.com>
2020-09-09 22:28:49 -0400

    Initial commit

---
+++ VideoGameGallery/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace VideoGameGallery.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
